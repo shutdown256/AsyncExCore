a03b2ff3b119c3d58f3c05289d6e74db1549baec
Vladislav Richter <ShutDown256@Gmail.com>
2016-06-16 21:30:48 +0200

    Initial commit

---
+++ src/Nito.AsyncEx/ExceptionHelpers.cs
@@ -0,0 +1,21 @@
+﻿using StudioMote.Components.AsyncEx.Internal.PlatformEnlightenment;
+using System;
+
+namespace StudioMote.Components.AsyncEx
+{
+    /// <summary>
+    /// Provides helper (non-extension) methods dealing with exceptions.
+    /// </summary>
+    public static class ExceptionHelpers
+    {
+        /// <summary>
+        /// Attempts to prepare the exception for re-throwing by preserving the stack trace. The returned exception should be immediately thrown.
+        /// </summary>
+        /// <param name="exception">The exception. May not be <c>null</c>.</param>
+        /// <returns>The <see cref="Exception"/> that was passed into this method.</returns>
+        public static Exception PrepareForRethrow(Exception exception)
+        {
+            return ExceptionEnlightenment.PrepareForRethrow(exception);
+        }
+    }
+}
---
+++ src/Nito.AsyncEx/Internal/PlatformEnlightenment/SingleThreadedApartmentThread.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StudioMote.Components.AsyncEx.Internal.PlatformEnlightenment
+{
+    public sealed class SingleThreadedApartmentThread
+    {
+        private readonly object _thread;
+
+        public SingleThreadedApartmentThread(Action execute, bool sta)
+        {
+            if (sta) throw new NotSupportedException();
+            _thread = sta ? new ThreadTask(execute) : (object)Task.Factory.StartNew(execute, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        public Task JoinAsync()
+        {
+            var ret = _thread as Task;
+            if (ret != null)
+                return ret;
+            return ((ThreadTask)_thread).Task;
+        }
+
+        private sealed class ThreadTask
+        {
+            private readonly TaskCompletionSource<object> _tcs;
+            private readonly Thread _thread;
+            public ThreadTask(Action execute)
+            {
+#if NET451
+                _tcs = new TaskCompletionSource<object>();
+                _thread = new Thread(() =>
+                {
+                    try
+                    {
+                        execute();
+                    }
+                    finally
+                    {
+                        _tcs.TrySetResult(null);
+                    }
+                });
+                _thread.SetApartmentState(ApartmentState.STA);
+                _thread.Name = "STA AsyncContextThread (Nito.AsyncEx)";
+                _thread.Start();
+#endif
+            }
+            public Task Task
+            {
+                get { return _tcs.Task; }
+            }
+        }
+    }
+}
